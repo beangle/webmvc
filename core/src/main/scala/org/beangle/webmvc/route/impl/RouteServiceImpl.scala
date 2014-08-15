2f463956bff8f0a2b4e4ddc87ddcccc04d2150e9
chaostone <duantihua@gmail.com>
2014-08-16 02:17:50 +0800

    change convention-route.properties  to xml format

--- core/src/main/scala/org/beangle/webmvc/route/impl/RouteServiceImpl.scala
+++ core/src/main/scala/org/beangle/webmvc/route/impl/RouteServiceImpl.scala
@@ -2,80 +2,91 @@ package org.beangle.webmvc.route.impl
 
 import java.lang.reflect.Method
 import java.net.URL
-import java.{ util => ju }
+
+import scala.xml.{ Node, XML }
+
 import org.beangle.commons.bean.PropertyUtils.{ copyProperty, getProperty }
-import org.beangle.commons.io.IOs
-import org.beangle.commons.lang.{ ClassLoaders, Strings }
+import org.beangle.commons.lang.ClassLoaders
 import org.beangle.commons.logging.Logging
-import org.beangle.webmvc.route.{ Action, ActionMapping, Profile, RouteService }
-import org.beangle.webmvc.route.StrutsAction
+import org.beangle.webmvc.route.{ ActionMapping, Profile, RouteService, StrutsAction }
 
 object RouteServiceImpl extends Logging {
 
   val defaultProfile = loadDefaultProfile()
 
   /**
-   * 初始化配置META-INF/convention-route.properties
+   * 初始化配置META-INF/convention-route.xml
    */
   def loadProfiles(): List[Profile] = {
     val profiles = new collection.mutable.ListBuffer[Profile]
-    ClassLoaders.getResources("META-INF/beangle/convention-route.properties").foreach { url =>
-      profiles ++= buildProfiles(url, false)
+    ClassLoaders.getResources("META-INF/beangle/convention-route.xml").foreach { url =>
+      profiles ++= readXmlToProfiles(url)
     }
     profiles.toList
   }
 
-  /**加载META-INF/convention-default.properties*/
+  /**加载META-INF/convention-default.xml*/
   private def loadDefaultProfile(): Profile = {
-    val convention_default = ClassLoaders.getResource("META-INF/beangle/convention-default.properties")
-    if (null == convention_default) throw new RuntimeException("cannot find convention-default.properties!")
-    buildProfiles(convention_default, true)(0)
+    val convention_default = ClassLoaders.getResource("META-INF/beangle/convention-default.xml")
+    if (null == convention_default) throw new RuntimeException("cannot find convention-default.xml!")
+    readXmlToProfiles(convention_default)(0)
   }
 
-  private def buildProfiles(url: URL, isDefault: Boolean): Seq[Profile] = {
-    val myProfiles = new collection.mutable.ListBuffer[Profile]
-    val props = IOs.readJavaProperties(url)
-    if (isDefault) {
-      val profile = populatProfile(props, "default")
-      myProfiles += profile
-    } else {
-      var profileIndex: Int = 0
-      var ifBreak = true
-      while (ifBreak) {
-        var profile = populatProfile(props, "profile" + profileIndex)
-        if (null == profile) {
-          ifBreak = false
-        } else {
-          myProfiles += profile
+  private def readXmlToProfiles(url: URL): Seq[Profile] = {
+    val profiles = new collection.mutable.ListBuffer[Profile]
+    XML.load(url) \ "profile" foreach { profileElem =>
+      val name = (profileElem \ "@name").text
+      val pattern = (profileElem \ "@pattern").text
+      val profile = new Profile(name, pattern)
+      val actionNodes = profileElem \ "action"
+      if (actionNodes.isEmpty) {
+        copyDefaultProperties(profile, "actionScan", "actionSuffix", "defaultMethod")
+      } else {
+        actionNodes foreach { elem =>
+          readProperty(elem, profile, "scan", "actionScan")
+          readProperty(elem, profile, "suffix", "actionSuffix")
+          readProperty(elem, profile, "defaultMethod", "defaultMethod")
         }
-        profileIndex += 1
       }
+
+      val viewNodes = profileElem \ "view"
+      if (viewNodes.isEmpty) {
+        copyDefaultProperties(profile, "viewPath", "viewPathStyle", "viewSuffix")
+      } else {
+        viewNodes foreach { elem =>
+          readProperty(elem, profile, "path", "viewPath")
+          readProperty(elem, profile, "style", "viewPathStyle")
+          readProperty(elem, profile, "suffix", "viewSuffix")
+        }
+      }
+
+      val uriNodes = profileElem \ "uri"
+      if (uriNodes.isEmpty) {
+        copyDefaultProperties(profile, "uriPath", "uriPathStyle", "uriSuffix")
+      } else {
+        uriNodes foreach { elem =>
+          readProperty(elem, profile, "path", "uriPath")
+          readProperty(elem, profile, "style", "uriPathStyle")
+          readProperty(elem, profile, "suffix", "uriSuffix")
+        }
+      }
+      profiles += profile
     }
-    myProfiles
+    profiles
   }
 
-  private def populatProfile(props: Map[String, String], name: String): Profile = {
-    val actionPattern = props.get(name + ".actionPattern").orNull
-    if (Strings.isEmpty(actionPattern)) null
-    else {
-      val profile = new Profile(name, actionPattern)
-      populateAttr(profile, "actionSuffix", props)
-      populateAttr(profile, "viewPath", props)
-      populateAttr(profile, "viewSuffix", props)
-      populateAttr(profile, "viewPathStyle", props)
-      populateAttr(profile, "defaultMethod", props)
-      populateAttr(profile, "uriPath", props)
-      populateAttr(profile, "uriPathStyle", props)
-      populateAttr(profile, "uriSuffix", props)
-      populateAttr(profile, "actionScan", props)
-      profile
+  private def readProperty(elem: Node, profile: Profile, attrName: String, propertyName: String): Unit = {
+    val xmlAttribute = "@" + attrName
+    if (!(elem \ xmlAttribute).isEmpty) {
+      copyProperty(profile, propertyName, (elem \ xmlAttribute).text)
+    } else {
+      copyProperty(profile, propertyName, getProperty(defaultProfile, propertyName))
     }
   }
 
-  private def populateAttr(profile: Profile, attr: String, props: Map[String, String]) {
-    props.get(profile.name + "." + attr) match {
-      case Some(v) => if (Strings.isBlank(v)) copyProperty(profile, attr, null) else copyProperty(profile, attr, v)
-      case None => copyProperty(profile, attr, getProperty(defaultProfile, attr))
+  private def copyDefaultProperties(profile: Profile, properties: String*): Unit = {
+    properties foreach { propertyName =>
+      copyProperty(profile, propertyName, getProperty(defaultProfile, propertyName))
     }
   }
 }
