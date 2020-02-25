ca6e79bc724a71c8e2037de9b7eee10eed5141b0
段体华 <duantihua@gmail.com>
2020-02-25 21:57:52 +0800

    update template 0.0.26

--- view/src/main/scala/org/beangle/webmvc/view/tag/freemarker/FreemarkerTemplateEngine.scala
+++ view/src/main/scala/org/beangle/webmvc/view/tag/freemarker/FreemarkerTemplateEngine.scala
@@ -29,7 +29,7 @@ import org.beangle.commons.io.IOs
 import org.beangle.commons.lang.annotation.description
 import org.beangle.commons.lang.{ClassLoaders, Throwables}
 import org.beangle.commons.logging.Logging
-import org.beangle.template.freemarker.BeangleClassTemplateLoader
+import org.beangle.template.freemarker.{BeangleClassTemplateLoader, IncludeIfExistsModel}
 import org.beangle.webmvc.api.context.ActionContext
 import org.beangle.webmvc.view.freemarker.{CachedObjectWrapper, FreemarkerModelBuilder}
 import org.beangle.webmvc.view.tag.{Component, TemplateEngine}
@@ -84,6 +84,7 @@ class FreemarkerTemplateEngine(modelBuilder: FreemarkerModelBuilder) extends Tem
 
     config.setCacheStorage(new StrongCacheStorage())
     config.setTagSyntax(Configuration.SQUARE_BRACKET_TAG_SYNTAX)
+    config.setSharedVariable("include_if_exists",new IncludeIfExistsModel)
     // Disable auto imports and includes
     config.setAutoImports(new ju.HashMap(0))
     config.setAutoIncludes(new ju.ArrayList(0))
